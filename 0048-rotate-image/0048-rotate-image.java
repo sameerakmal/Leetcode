a4c8d121748dae32a0fffaccbd636a95346cc05f
Syed Sameer Akmal <154409904+sameerakmal@users.noreply.github.com>
2026-05-04 21:49:39 +0530

    Time: 0 ms (100%), Space: 43.8 MB (33.41%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            int end = n-1;
+            for(int j = 0; j < n/2; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][end];
+                mat[i][end--] = temp;
+            }
+        }
+    }
+}
+
